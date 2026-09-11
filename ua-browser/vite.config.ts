2f8740a67ba9c126b5bcd60131969d6854912ef1
Rémi Thebault <remi.thebault@gmail.com>
2026-09-11 06:53:19 +0200

    adding tailwind

--- ua-browser/vite.config.ts
+++ ua-browser/vite.config.ts
@@ -1,9 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   server: {
     watch: {
       ignored: ["**/src-tauri/**"],
